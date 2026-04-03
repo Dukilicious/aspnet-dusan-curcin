5c02cce1e0c14c97b39dce1ab4393cd0a6c1364f
Duki <dukilicious@hotmail.com>
2026-04-03 13:47:47 +0200

    refactor(headerUI): temporary styling for header

--- CoreFitness.Presentation/obj/Debug/net10.0/CoreFitness.Presentation.AssemblyInfo.cs
+++ CoreFitness.Presentation/obj/Debug/net10.0/CoreFitness.Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreFitness.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58bc5ed1072f535bdfaed255e5b38aa50f0df6ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreFitness.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreFitness.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
